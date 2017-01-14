b9b54de571b5e42f9717f9784c6dd7d338fff5ae
Sara Vieira <sara.vieira@mindera.com>
2017-01-14 18:43:08 +0000

    Add minification and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
-var serve = require('gulp-serve');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
-gulp.task('serve', serve('./'));
+var processors = [
+    autoprefixer({browsers: ['last 3 versions', 'last 4 iOS versions']}),
+    cssnano(),
+];
+
 gulp.task('less', function () {
   return gulp.src('./src/flexy.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./dist'));
 });
